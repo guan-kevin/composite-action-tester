f348df38f3be848b06ce3810a22cae5a94c64629
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-10-26 15:47:15 -0700

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,10 +1,3 @@
-if [[ $INPUT_TEST_INPUT == 1 ]]; then
-	echo "Input in test.sh OK"
-else
-	printenv
-	echo "Bad input in test.sh" && exit 1
-fi
-
 if [[ $LARGER_THAN_0 == 1 ]]; then
 	echo "Environment variable in test.sh OK"
 else
