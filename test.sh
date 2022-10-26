13e80b9e631f2975ba975833dbd0ebbf8e9971f3
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-10-26 15:38:02 -0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,13 @@
+if [[ $test_input == 1 ]]; then
+	echo "Input in test.sh OK"
+else
+	echo "Bad input in test.sh" && exit 1
+fi
+
+if [[ $LARGER_THAN_0 == 1 ]]; then
+	echo "Environment variable in test.sh OK"
+else
+	echo "Bad environment variable in test.sh"
+fi
+
+echo "Test OK!"
